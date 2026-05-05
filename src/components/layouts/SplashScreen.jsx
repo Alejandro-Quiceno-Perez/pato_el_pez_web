72749e27fcd6e51272b3da4b233aec0276085a8a
Alejandro-Quiceno-Perez <alejo.perez2905@gmail.com>
2026-05-05 11:12:45 -0500

    Se crea funcionalidad de Splash Screem queda funcional

--- src/components/layouts/SplashScreen.jsx
+++ src/components/layouts/SplashScreen.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import "../../styles/SplashScreem.css"
 
 const SplashScreen = () => {
+    const splashScreem = '/videos/splashScreem.mp4';
+
+    const [fadeOut, setFadeOut] = useState(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setFadeOut(true);
+        }, 8000); // 8 seconds
+
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
-        <div>
-            SplashScreen
+        <div className={`splash-container ${fadeOut ? 'fade-out' : ''}`}>
+            <video className="splash-video" autoPlay muted>
+                <source src={splashScreem} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
         </div>
     )
 }
