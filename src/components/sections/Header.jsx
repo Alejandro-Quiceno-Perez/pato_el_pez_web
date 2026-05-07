9b111127e18bbc1a5f41921b8d2fb076c5ebc3dd
Alejandro Quiceno Perez <alejo.perez2905@gmail.com>
2026-05-07 14:58:07 -0500

    Se crea el componente header con sus respectivos campos, tambien se añade funcionalidad del header para navegacion con las otras paginas, se hace un cambio de estructura interna al proyecto

--- src/components/sections/Header.jsx
+++ src/components/sections/Header.jsx
@@ -1,10 +1,50 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import logo from '/public/logo_letras_negro.png'
 
 const Header = () => {
+    const leftNav = [
+        { name: "Videos", path: "/videos" },
+        { name: "Musica", path: "/music" },
+        { name: "Recorrido", path: "/recorrido" },
+    ];
+
+    const rightNav = [
+        { name: "Tienda", path: "/merch" },
+        { name: "Fans", path: "/subscribe" },
+        { name: "Press Kit", path: "/press-kit" },
+    ];
+
+    const NavGroup = ({ items }) => {
+        return (
+            <>
+                {items.map((item, index) => (
+                    <Link key={index} to={item.path}>{item.name}</Link>
+                ))}
+            </>
+        )
+    }
+
+
     return (
-        <div>
-            Header
-        </div>
+        <header className='header-container'>
+            {/* Lado Izquierdo  */}
+            <nav className='nav-left'>
+                <NavGroup items={leftNav} />
+            </nav>
+
+            {/* Logo */}
+            <div className='logo'>
+                <Link to="/">
+                    <img src={logo} alt="Pato el Pez logo" />
+                </Link>
+            </div>
+
+            {/* Lado Derecho */}
+            <nav className='nav-right'>
+                <NavGroup items={rightNav} />
+            </nav>
+        </header>
     )
 }
 
