e20304749548e2c07d8bd6513465c5587c739a14
Alejandro Quiceno <alejo.perez2905@gmail.com>
2026-05-27 15:50:19 -0500

    Actualizaciones en la bd de music

--- src/db/Music.js
+++ src/db/Music.js
@@ -2,11 +2,101 @@ const Music = [
     {
         "id": 1,
         "title": "Mejor Que Esto",
-        "cover": "/music/song.jpg",
+        "cover": "/public/images/img_music/Mejor_que_esto.jpg",
         "spotify": "",
         "youtube": "",
         "releaseDate": "2025-04-12",
         "lyrics": "",
         "duration": "3:42"
-    }
+    },
+    {
+        "id": 2,
+        "title": "Xiomi",
+        "cover": "/public/images/img_music/Xiomi.jpg",
+        "spotify": "",
+        "youtube": "",
+        "releaseDate": "2025-04-12",
+        "lyrics": "",
+        "duration": "3:42"
+    },
+    {
+        "id": 3,
+        "title": "Dejarte Ir",
+        "cover": "/public/images/img_music/Dejarte_ir.jpg",
+        "spotify": "",
+        "youtube": "",
+        "releaseDate": "2025-04-12",
+        "lyrics": "",
+        "duration": "3:42"
+    },
+    {
+        "id": 4,
+        "title": "Rutina",
+        "cover": "/public/images/img_music/Rutina.jpg",
+        "spotify": "",
+        "youtube": "",
+        "releaseDate": "2025-04-12",
+        "lyrics": "",
+        "duration": "3:42"
+    },
+    {
+        "id": 5,
+        "title": "Cuando Sea Grande",
+        "cover": "/public/images/img_music/Cuando_sea_grande.jpg",
+        "spotify": "",
+        "youtube": "",
+        "releaseDate": "2025-04-12",
+        "lyrics": "",
+        "duration": "3:42"
+    },
+    {
+        "id": 6,
+        "title": "La Bestia",
+        "cover": "/public/images/img_music/La_bestia.jpg",
+        "spotify": "",
+        "youtube": "",
+        "releaseDate": "2025-04-12",
+        "lyrics": "",
+        "duration": "3:42"
+    },
+    {
+        "id": 7,
+        "title": "La Oveja Negra",
+        "cover": "/public/images/img_music/La_oveja_negra.jpg",
+        "spotify": "",
+        "youtube": "",
+        "releaseDate": "2025-04-12",
+        "lyrics": "",
+        "duration": "3:42"
+    },
+    {
+        "id": 8,
+        "title": "22",
+        "cover": "/public/images/img_music/22.jpg",
+        "spotify": "",
+        "youtube": "",
+        "releaseDate": "2025-04-12",
+        "lyrics": "",
+        "duration": "3:42"
+    },
+    {
+        "id": 9,
+        "title": "Esto Es Pato",
+        "cover": "/public/images/img_music/Esto_es_pato.jpg",
+        "spotify": "",
+        "youtube": "",
+        "releaseDate": "2025-04-12",
+        "lyrics": "",
+        "duration": "3:42"
+    },
+    {
+        "id": 10,
+        "title": "El Pobre Sebas",
+        "cover": "/public/images/img_music/El_pobre_sebas.jpg",
+        "spotify": "",
+        "youtube": "",
+        "releaseDate": "2025-04-12",
+        "lyrics": "",
+        "duration": "3:42"
+    },
 ]
