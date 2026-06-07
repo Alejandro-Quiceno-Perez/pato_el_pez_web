196bebdd171da161cc91f1f6d7adbd627fb480df
Alejandro-Quiceno-Perez <alejo.perez2905@gmail.com>
2026-06-07 12:38:50 -0500

    Se crea el banner de bienvenida, aun esta sujeto a muchos cambios esteticos, se agregan documentos de EPK, RIDER, BROCHURE

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import BannerBienvenida from '../components/UI/BannerBienvenida'
 
 const Home = () => {
     return (
         <div>
-            Home
+            <BannerBienvenida />
         </div>
     )
 }
