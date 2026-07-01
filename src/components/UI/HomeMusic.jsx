239bbc4b73346413954d5842b37360a15a05d82d
Alejandro Quiceno <alejo.perez2905@gmail.com>
2026-07-01 11:58:45 -0500

    Se actualiza el feature/homeMusic aun no se termina debido a cambios y actualizaciones

--- src/components/UI/HomeMusic.jsx
+++ src/components/UI/HomeMusic.jsx
@@ -8,33 +8,41 @@ const imgAlbum = '/img_music/Caratula_disco.jpg'
 const HomeMusic = () => {
     const featuredSongs = Songs.slice(0, 5) // Get the first 5 songs as featured songs
     return (
-        <section className='home-music-container'>
-            <div className="container-info-musica">
+        < section className="home-music-container" >
+
+            <div className="music-wrapper">
+
                 <div className="album-cover">
-                    <img src={imgAlbum} alt="Caratula del disco" />
+                    <img src={imgAlbum} alt="" />
                 </div>
+
                 <div className="music-section-title">
-                    <span>Último Lanzamiento</span>
-                    <h2>Nuestra música</h2>
-                    <p>Nuestro primer álbum de estudio, 10 canciones que son el reflejo de todo lo que somos y de todo lo que hemos vivido</p>
+                    <span>Último lanzamiento</span>
+
+                    <h2>La Oveja Negra</h2>
+
+                    <p>
+                        Nuestro primer álbum de estudio, 10 canciones que son el
+                        reflejo de todo lo que somos y de todo lo que hemos vivido.
+                    </p>
+
                     <div className="music-section-link">
-                        <a href="https://open.spotify.com/artist/4pbnupXCtNilC05RqeFrCO?si=M-p4hvrWSj-JCx-sck4tWw">Escuchar ahora</a>
+                        <a href="">Escuchar ahora</a>
                         <a href="/music">Ver más</a>
                     </div>
                 </div>
+            </div>
 
-            </div>
             <div className="songs-grid">
-                {
-                    featuredSongs.map(song => (
-                        <SongCard
-                            key={song.id}
-                            song={song}
-                        />
-                    ))
-                }
+                {featuredSongs.map(song => (
+                    <SongCard
+                        key={song.id}
+                        song={song}
+                    />
+                ))}
             </div>
-        </section>
+
+        </section >
     )
 }
 
