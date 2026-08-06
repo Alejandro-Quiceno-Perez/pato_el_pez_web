8dbcb8e8a55c5227fd3fad31eeedca747c855746
Alejandro-Quiceno-Perez <alejo.perez2905@gmail.com>
2026-08-06 12:41:01 -0500

    Se hace un cambio especifico en el SplashScreem y se agrega un nuevo video para el modo movile

--- src/components/UI/SplashScreen.jsx
+++ src/components/UI/SplashScreen.jsx
@@ -5,7 +5,7 @@ const DISPLAY_DURATION = 7200;
 const FADE_DURATION = 800;
 
 const SplashScreen = ({ onFinish }) => {
-    const splashScreem = '/videos/splashScreem.mp4';
+    // const splashScreem = '/videos/splashScreem.mp4';
 
     const [fadeOut, setFadeOut] = useState(false);
 
@@ -26,12 +26,37 @@ const SplashScreen = ({ onFinish }) => {
 
     return (
         <div className={`splash-container ${fadeOut ? 'fade-out' : ''}`}>
-            <video className="splash-video" autoPlay muted>
-                <source src={splashScreem} type="video/mp4" />
-                Your browser does not support the video tag.
+
+            {/* Desktop */}
+
+            <video
+                className="splash-video splash-desktop"
+                autoPlay
+                muted
+                playsInline
+            >
+                <source
+                    src="/videos/splashScreem.mp4"
+                    type="video/mp4"
+                />
             </video>
+
+            {/* Mobile */}
+
+            <video
+                className="splash-video splash-mobile"
+                autoPlay
+                muted
+                playsInline
+            >
+                <source
+                    src="/videos/SplashScreemCelular.mp4"
+                    type="video/mp4"
+                />
+            </video>
+
         </div>
-    )
+    );
 }
 
 export default SplashScreen
