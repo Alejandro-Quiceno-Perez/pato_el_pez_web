6509b35b5eafcd34c32fd5f7cf7a5de985740c41
AlejandroQuiceno <alejo.perez2905@gmail.com>
2026-09-11 00:32:45 -0500

    Se hacen actalizacion de run deploy

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
-  base: "/pato_el_pez_web/",
-  server: {
-    watch: {
-      usePolling: true,
-      interval: 100,
+    plugins: [react()],
+
+    base: '/',
+
+    server: {
+        watch: {
+            usePolling: true,
+            interval: 100,
+        },
     },
-  },
 })
