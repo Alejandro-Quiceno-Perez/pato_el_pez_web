ff514e8ebd47ca2e96404c6d3f53e396cb7c1339
AlejandroQuiceno <alejo.perez2905@gmail.com>
2026-09-23 13:44:35 -0500

    Actualizaciones en rutas para GoogleAnalytics

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,48 +1,43 @@
-import { lazy, Suspense } from 'react'
+import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import PageLoader from '../components/UI/PageLoader';
+import PageLoader from "../components/UI/PageLoader";
+import GoogleAnalytics from "../components/GoogleAnalytics/GoogleAnalytics";
 const Home = lazy(() => import("../pages/Home"));
 const Music = lazy(() => import("../pages/Music"));
 const Merch = lazy(() => import("../pages/Merch"));
 const Videos = lazy(() => import("../pages/Videos"));
 const PressKit = lazy(() => import("../pages/PressKit"));
-const Recorrido = lazy(() => import('../pages/Recorrido'));
-const Layout = lazy(() => import('../components/layouts/layout'));
-const Fans = lazy(() => import('../pages/Fans'));
+const Recorrido = lazy(() => import("../pages/Recorrido"));
+const Layout = lazy(() => import("../components/layouts/layout"));
+const Fans = lazy(() => import("../pages/Fans"));
 
 const AppRoutes = () => {
-    return (
-        <Router basename={import.meta.env.BASE_URL}>
+  return (
+    <Router basename={import.meta.env.BASE_URL}>
+      <GoogleAnalytics />
 
-            <Suspense fallback={<PageLoader />}>
+      <Suspense fallback={<PageLoader />}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
 
-                <Routes>
+            <Route path="music" element={<Music />} />
 
-                    <Route path="/" element={<Layout />}>
+            <Route path="merch" element={<Merch />} />
 
-                        <Route index element={<Home />} />
+            <Route path="videos" element={<Videos />} />
 
-                        <Route path="music" element={<Music />} />
+            <Route path="press-kit" element={<PressKit />} />
 
-                        <Route path="merch" element={<Merch />} />
+            <Route path="fans" element={<Fans />} />
 
-                        <Route path="videos" element={<Videos />} />
+            <Route path="recorrido" element={<Recorrido />} />
+          </Route>
+        </Routes>
+      </Suspense>
+    </Router>
+  );
+};
 
-                        <Route path="press-kit" element={<PressKit />} />
-
-                        <Route path="fans" element={<Fans />} />
-
-                        <Route path="recorrido" element={<Recorrido />} />
-
-                    </Route>
-
-                </Routes>
-
-            </Suspense>
-
-        </Router>
-    )
-}
-
-export default AppRoutes
+export default AppRoutes;
